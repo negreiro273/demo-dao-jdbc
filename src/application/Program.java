a1643793ba46acfa0afcb21f62644b9a9037fdb0
Uemerson Negreiro da Silva <90913370100@sinfra.mt.gov.br>
2024-11-18 14:49:10 -0400

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package demo.dao.jdbc;
+package application;
 
-/**
- *
- * @author 90913370100
- */
-public class DemoDaoJdbc {
+import model.entities.Department;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Program {
+
     public static void main(String[] args) {
         // TODO code application logic here
+        Department obj = new Department(1, "Bools");
+        System.out.println(obj.toString());
+        
+        
     }
     
 }
